a7d1cc0e229f030741bb142dba63d1b2424157e0
Gordon Hennesy <gordonhennesy@gmail.com>
2020-10-03 14:28:29 -0700

    Add the Model, Migration, Factory, Seeder, Controller

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Contact::factory()
+            ->times(50)
+            ->create();
+    }
+}
