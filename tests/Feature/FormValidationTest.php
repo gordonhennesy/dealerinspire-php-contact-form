63d8572be9eccc63aba24fd5886c89b16f9ac679
Gordon Hennesy <gordonhennesy@gmail.com>
2020-10-06 10:28:35 -0700

    Form Validation PHPUnit added

---
+++ tests/Feature/FormValidationTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FormValidationTest extends TestCase
+{
+    /**
+     * Validate Fullname form field has a value 
+     *
+     * @return void
+     */
+    public function testFormValidationNameNotNullTest()
+    {
+        $response = $this->post('contacts/store', 
+            [
+            'fullname'   => '', // Must be non-null
+            'email'      => 'name@example.com',
+            'phone'      => '(541) 555-1212',
+            'message'    => 'Lorem ipsum',
+            ]
+        );
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['fullname']);
+    }
+    public function testFormValidationEmailNotNullTest()
+    {
+        $response = $this->post('contacts/store', 
+            [
+            'fullname'   => 'Gordon Hennesy', 
+            'email'      => '', // Must be non-null
+            'phone'      => '(541) 555-1212',
+            'message'    => 'Lorem ipsum',
+            ]
+        );
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['email']);
+    }
+    public function testFormValidationMessageNotNullTest()
+    {
+        $response = $this->post('contacts/store', 
+            [
+            'fullname'   => 'Gordon Hennesy',
+            'email'      => 'name@example.com',
+            'phone'      => '(541) 555-1212',
+            'message'    => '', // Must be non-null
+            ]
+        );
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['message']);
+    }
+    public function testFormValidationPassesTest()
+    {
+        $response = $this->post('contacts/store', 
+            [
+            'fullname'   => 'Gordon Hennesy', // Must be non-null
+            'email'      => 'name@example.com',
+            'phone'      => '(541) 555-1212',
+            'message'    => 'Lorem ipsum',
+            ]
+        );
+
+        $response->assertStatus(302);
+        $response->assertSessionDoesntHaveErrors(
+            [
+                'fullname',
+                'email',
+                'message',
+            ]);
+    }
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
