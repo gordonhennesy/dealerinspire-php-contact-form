692f62c8a120ba288c41b296f388c19ef6988a35
Gordon Hennesy <gordonhennesy@gmail.com>
2020-10-07 10:03:44 -0700

    Test mail, save config of phpunit.xml

---
+++ app/Mail/TestMailer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+class TestMailer extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('example@example.com')
+        ->view('emails.mail', ['name'=>"Gambleputty", 'body'=>"<h1>Sent</h1>"]);
+        //return $this->view('view.name');
+    }
+}
---
+++ tests/Feature/MailerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMailer;
+
+class MailerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    public function testMailerTest()
+    {
+        Mail::to('elmer@example.com')->send(new TestMailer);
+
+        $this->assertTrue(True);
+    }
+
+}
